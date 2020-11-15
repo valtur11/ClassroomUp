97da2972b928e2d0131490aac614e14afa9cbd14
Valentin Ratchev <valentinratchev@gmail.com>
2020-11-15 13:03:57 +0200

    Apply eslint style guide

--- src/Components/layout.js
+++ src/Components/layout.js
@@ -1,21 +1,22 @@
-import './layout.css';
-import logo from './logo.svg';
+import './Layout.css';
 
 function Layout() {
-    return (
-        <div className="Layout">
-            <nav className="Layout">
-                <div class="NavBar">
-                    <a class="active" href="">Home</a>
-                    <a href="">App</a>
-                    <a href="">etc</a>
-                </div>
-            </nav>
-            <body>
-            </body>
-            <footer>
-                <img src={logo} className="Logo" alt="logo" />     
-</footer>
+  return (
+    <div className="Layout">
+      <nav className="Layout">
+        <div className="NavBar">
+          <a className='active' href='#'>Home</a>
+          <a href="">App</a>
+          <a href="">etc</a>
         </div>
-    );
+      </nav>
+      <body>
+      </body>
+      <footer>
+        <img src='/logo.png' className="Logo" alt="logo" />
+      </footer>
+    </div>
+  );
 }
+
+export default Layout;
