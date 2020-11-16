1e1005bbf178a2b9e64cdf70c526dd0a1f2a75a0
Valentin Ratchev <valentinratchev@gmail.com>
2020-11-16 06:09:40 +0200

    Fix image size

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,16 +3,20 @@ import Layout from './Layout';
 function Home() {
   return (
     <Layout>
-      <div className="row">
-        <div className="col" style={{backgroundColor: '#FFFFFF'}}>
-          <h2>Our Project</h2>
-          <p>Classroom UP was built to resolve some major issues in remote learning, especially the Video call. Our App is made to handle 3 major problems with Video call apps, Inconvenince when taking attendence for teachers, Students forgetting to attend the meeting, and the distractions caused by unmuting microphones to ask questions during a test.</p>
+      <div className='container-fluid'>
+        <div className="row">
+          <div className="col-12" style={{backgroundColor: '#FFFFFF'}}>
+            <h2>Our Project</h2>
+            <p>Classroom UP was built to resolve some major issues in remote learning, especially the Video call. Our App is made to handle 3 major problems with Video call apps, Inconvenince when taking attendence for teachers, Students forgetting to attend the meeting, and the distractions caused by unmuting microphones to ask questions during a test.</p>
+          </div>
         </div>
-        <div className="col" style={{backgroundColor: '#FFFFFF'}}>
-          <h2>Our Story</h2>
-          <p>After meeting at an Online Hackathon, We all agreed that remote learning is difficult, and this app exemplifies the ideas that we came up with to make it better.</p>
-          <h3>Here is our team:</h3>
-          <img src='./Team.PNG' className="Logo" alt="logo" />
+        <div className="row">
+          <div className="col-12" style={{backgroundColor: '#FFFFFF'}}>
+            <h2>Our Story</h2>
+            <p>After meeting at an Online Hackathon, We all agreed that remote learning is difficult, and this app exemplifies the ideas that we came up with to make it better.</p>
+            <h3>Here is our team:</h3>
+            <img src='./Team.PNG' width= '400px' height='200px' className="Logo" alt="logo" />
+          </div>
         </div>
       </div>
     </Layout>
